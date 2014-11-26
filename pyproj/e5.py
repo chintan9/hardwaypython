3d572576fde0995e6310c2243591211c543a57c2
chintan9 <chintanbprajapati@gmail.com>
2014-11-26 09:04:17 +0000

    init

---
+++ pyproj/e5.py
@@ -0,0 +1,13 @@
+my_name = 'CHINTAN'
+my_age = 23
+my_height = 74
+my_weight = 180
+my_eyes = 'BLACK'
+my_teeth = 'White'
+print "Let's Talk about %s. " % my_name
+print "He's %d inches tall." % my_height
+print "He's %d pounds heavy " % my_weight
+print "Actually that's not too heavy."
+print "He's got %s eyes and hair."
+print "His teeth are usually %s depending on the coffee." % my_teeth
+print "If I add %d , %d, and %d I get %d." % ( my_age, my_height, my_weight, my_age + my_height + my_weight )
